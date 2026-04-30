8a4370b433a8c1ec69080ad265a57505fee6a7f6
Rashky Jauhara <rikyjauhara@gmail.com>
2026-04-30 20:15:49 +0700

    fix: improve the train logic to produce maximum results

--- src/train.py
+++ src/train.py
@@ -13,14 +13,18 @@ MODEL_DIR = 'models/'
 def train_model():
     print("Memulai proses Model Training dengan MLflow ...")
     
-    # 1. Load Data Processed
+    # Load Data Processed
     if not os.path.exists(PROCESSED_DATA_PATH):
         print(f"Error: Data {PROCESSED_DATA_PATH} tidak ditemukan.")
         return
         
     df = pd.read_csv(PROCESSED_DATA_PATH)
+
+    # Konversi kembali kolom Store menjadi tipe data 'category'
+    print("Mengonversi tipe data Store menjadi category...")
+    df['Store'] = df['Store'].astype('category')
     
-    # 2. Time Series Split (Sorting kronologis)
+    # Time Series Split (Sorting kronologis)
     print("Mempersiapkan data dan melakukan time-series split...")
     df = df.sort_values(by=['Year', 'Month', 'Day'])
     
@@ -32,12 +36,16 @@ def train_model():
     X_train, X_valid = X.iloc[:split_index], X.iloc[split_index:]
     y_train, y_valid = y.iloc[:split_index], y.iloc[split_index:]
     
-    # 3. Inisialisasi Eksperimen MLflow
-    # Ini akan membuat folder mlruns/ jika dijalankan secara lokal
+    # Log tranformation pada target 
+    print("Menerapkan transformasi logaritmik pada variabel target...")
+    y_train_log = np.log1p(y_train)
+    y_valid_log = np.log1p(y_valid)
+
+    # Inisialisasi Eksperimen MLflow (akan membuat folder mlruns/ jika dijalankan secara lokal)
     mlflow.set_experiment("Retail_Demand_Forecasting")
     
     # Mulai pencatatan eksperimen
-    with mlflow.start_run(run_name="Run#5"):
+    with mlflow.start_run(run_name="Run#1"):
         
         # Definisikan Hyperparameter
         params = {
@@ -45,46 +53,49 @@ def train_model():
             'learning_rate': 0.05,
             'max_depth': 4,
             'random_state': 42,
-            'objective': 'reg:squarederror'
+            'objective': 'reg:squarederror',
+            'enable_categorical': True
         }
         
-        # --- MLFLOW: MENCATAT PARAMETER ---
+        # MLFlow mencatat parameter
         print("Mencatat parameter ke MLflow...")
         for key, value in params.items():
             mlflow.log_param(key, value)
             
-        # 4. Melatih Model XGBoost
-        print("Sedang melatih model... (Mohon tunggu)")
+        # Melatih Model XGBoost
+        print("Sedang melatih model...")
         xgb_model = xgb.XGBRegressor(**params)
         
+        # Training menggunakan target yang sudah di-log
         xgb_model.fit(
-            X_train, y_train,
-            eval_set=[(X_train, y_train), (X_valid, y_valid)],
+            X_train, y_train_log,
+            eval_set=[(X_train, y_train_log), (X_valid, y_valid_log)],
             verbose=False # Matikan log bawaan XGBoost agar terminal tetap bersih
         )
         
-        # 5. Prediksi dan Evaluasi
+        # Prediksi dan Evaluasi
         print("Mengevaluasi model pada data validasi...")
-        y_pred = xgb_model.predict(X_valid)
+        y_pred_log = xgb_model.predict(X_valid) # Hasil prediksi masih dalam bentuk logaritmik
         
+        # Kembalikan ke nilai asli (inverse transform) sebelum menghitung error
+        y_pred = np.expm1(y_pred_log)
+
         rmse = np.sqrt(mean_squared_error(y_valid, y_pred))
         mape = mean_absolute_percentage_error(y_valid, y_pred)
         
-        # --- MLFLOW: MENCATAT METRIK ---
+        # MLFlow mencatat metrik
         print("Mencatat metrik ke MLflow...")
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("mape", mape)
         
         print(f"Validation RMSE: {rmse:.2f}")
-        # Cetak MAPE dalam format persentase yang mudah dibaca
         print(f"Validation MAPE: {mape:.2%}") 
         
-        # --- MLFLOW: MENCATAT MODEL ARTIFAK ---
+        # MLFlow mencatat model artefak
         print("Menyimpan model ke MLflow...")
         mlflow.xgboost.log_model(xgb_model, "xgboost_model")
         
-        # 6. Menyimpan model secara fisik untuk DVC Pipeline
-        # DVC membutuhkan file fisik untuk di-track dalam dvc.yaml outs
+        # Menyimpan model secara fisik untuk DVC Pipeline
         os.makedirs(MODEL_DIR, exist_ok=True)
         local_model_path = os.path.join(MODEL_DIR, 'xgboost_sales_model.json')
         xgb_model.save_model(local_model_path)
